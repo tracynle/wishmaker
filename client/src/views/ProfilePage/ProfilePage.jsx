90a8b7183794d8355c66df78b07742398f5f6c99
Tracy Le <tnle9@asu.edu>
2019-05-26 21:49:17 -0700

    Edited all paths

--- client/src/views/ProfilePage/ProfilePage.jsx
+++ client/src/views/ProfilePage/ProfilePage.jsx
@@ -58,7 +58,7 @@ class ProfilePage extends React.Component {
           }}
           {...rest}
         />
-        <Parallax small filter image={require("assets/img/profile-bg.jpg")} />
+        <Parallax small filter image={require("../../assets/img/profile-bg.jpg")} />
         <div className={classNames(classes.main, classes.mainRaised)}>
           <div>
             <div className={classes.container}>
